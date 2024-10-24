5d5f9b99944e10d3b097bf1ed26e6ffb4a740b4b
Sawyerf <sawyer.flink@protonmail.ch>
2024-10-24 16:20:15 +0200

    Clean: Python

--- Python/app.py
+++ Python/app.py
@@ -4,30 +4,35 @@ import os
 
 app = Flask(__name__)
 
+def read_yaml():
+    with open('config.yaml') as file:
+        config = yaml.safe_load(file)
+        return config
+
+def write_yaml(config):
+    with open('config.yaml', 'w+') as file:
+        yaml.dump(config, file)
+
 @app.get("/config")
 def config_get():
     if os.path.exists('config.yaml') == False:
         return {'error':'File not found'}, 404
     try:
-        with open('config.yaml') as file:
-            config = yaml.load(file, Loader=yaml.FullLoader)
-            # config = file.read()
-            return config
+        return read_yaml()
     except:
         return {'error': 'Error reading file'}, 404
 
 @app.put("/config/<string:key>/<string:var>")
 def config_put(key, var):
-    print(key, var)
     body = request.get_json()
     if var not in body:
         return {'error': 'variable not found'}, 400
-    with open('config.yaml', 'w+') as file:
-        config = yaml.load(file, Loader=yaml.FullLoader)
-        if config is None:
-            config = {}
-        if key not in config:
-            config[key] = {}
-        config[key][var] = body[var]
-        yaml.dump(config, file)
-        return {'success': 'variable updated'}, 200
+    
+    config = read_yaml()
+    if config is None:
+        config = {}
+    if key not in config:
+        config[key] = {}
+    config[key][var] = body[var]
+    write_yaml(config)
+    return {'success': 'variable updated'}, 200
